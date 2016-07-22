f16c6db2db1b2e42a0c81cc536f2b30145c915d4
Carson Roscoe <CarsonRoscoe7@gmail.com>
2016-07-22 11:50:02 -0700

    First draft design work

---
+++ High10/High10/ExtensionMethods/PrismExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace High10.ExtensionMethods {
+    public static class PrismExtensionMethods {
+        public static string CreateURI(this INavigationService @this, params string[] pages) {
+            var result = "";
+            foreach (var item in pages) {
+                result += item + "/";
+            }
+            return result;
+        }
+    }
+}
