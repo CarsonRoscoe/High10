a3641c473edaf1cec6bff6488140bb457fb6b473
Carson Roscoe <CarsonRoscoe7@gmail.com>
2016-08-10 21:29:02 -0700

    Created LongPressListView & Android renderer for it

---
+++ High10/High10/CustomControls/LongPressListView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace High10.CustomControls {
+  // NOTE: Allows Android to bypass the action bar implementation of ContextActions via a custom renderer.
+  // iOS can use ContextActions as usual
+  // Create subcasses for each ViewModel combination
+  public abstract class LongPressListView : ListView {
+    public abstract void HandleItemLongPressed( int index );
+  }
+}
